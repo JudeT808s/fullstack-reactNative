e7bf487b0d91ea995264272b473cfd20ebe2b1ce
jude <jaytbas@gmail.com>
2024-10-18 19:43:00 +0100

    made a fight with nativewind docs

--- app/index.jsx
+++ app/index.jsx
@@ -5,11 +5,14 @@ import {Link} from 'expo-router';
 
 export default function App() {
   return (
-   <View className= " bg-red">
+    <>
+   <View className= "flex-1 items-center justify-center bg-white">
     <Text className='text-3xl font-pblack'>Aora</Text>
     <StatusBar style="auto" />
-    <Link href="/home" style={{color:'blue'}}>Go to Home</Link>
+    <Link href="/home" style={{color:'red'}}>Go to Home</Link>
     </View>
+    <Text className="text-bold">Hi</Text>
+    </>
   );
 
 }
