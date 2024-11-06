cf87aefe81d4b4eb1d7984c79e4358ed1a0007aa
jude <jaytbas@gmail.com>
2024-11-06 19:36:40 +0000

    Refactor Trending component and update dependencies in home.jsx and Trending.jsx

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -1,66 +1,37 @@
-import { View, Text, FlatList } from "react-native";
 import { useState } from "react";
-import React from "react";
-import * as AnimaTable from "react-native-animatable";
-import { TouchableOpacity } from "react-native";
-import { ImageBackground } from "react-native-web";
-const TrendingItem = ({ activeItem, item }) => {
-  const [play, setPlay] = useState(false);
+import { ResizeMode, Video } from "expo-av";
+import * as Animatable from "react-native-animatable";
+import {
+  FlatList,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 
-  return (
-    <AnimaTable.View
-      className="mr-5"
-      animation={activeItem == item.$id ? zoomIn : zoomOut}
-      duration={500}
-    >
-      {play ? (
-        <Text className="text-3xl text-white">Playing</Text>
-      ) : (
-        <TouchableOpacity className="relative justify-center items-center" activeOpacity={0.7}onPress={() => setPlay(true)}>
-          <ImageBackground
-            source={{ uri: item.thumbnail }}
-            className="w-52 h-72 rounded-[35px] overflow-hidden shadow-lg shadow-black/40"
-          />
-        </TouchableOpacity>
-      )}
-    </AnimaTable.View>
-  );
-  const zoomIn = {
-    0: {
-      scale: 0,
-    },
-    0.5: {
-      scale: 0.5,
-    },
-    1: {
-      scale: 1,
-    },
-  };
-  const zoomOut = {
-    0: {
-      scale: 1,
-    },
-    0.5: {
-      scale: 0.5,
-    },
-    1: {
-      scale: 0.9,
-    },
-  };
+import { icons } from "../constants";
+
+const zoomIn = {
+  0: {
+    scale: 0.9,
+  },
+  1: {
+    scale: 1,
+  },
 };
-const Trending = (posts) => {
-  const [activeItem, setActiveItem] = useState(posts[0]);
 
-  return (
-    <FlatList
-      data={posts}
-      keyExtractor={(item) => item.$id}
-      renderItem={({ item }) => (
-        <Text className="text-3xl text-white">{item.id}</Text>
-      )}
-      horizontal
-    />
-  );
+const zoomOut = {
+  0: {
+    scale: 1,
+  },
+  1: {
+    scale: 0.9,
+  },
+};
+
+const TrendingItem = ({ activeItem, item }) => {
+};
+
+const Trending = ({ posts }) => {
 };
 
 export default Trending;
